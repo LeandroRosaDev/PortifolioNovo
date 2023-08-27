edb97b2b169d4c505f50a028d807759bd0592b7a
LeandroRosaDev <thivithale@hotmail.com>
2023-08-27 12:22:59 -0300

    Adicionando novos icones de redes sociais

--- script.js
+++ script.js
@@ -3,9 +3,17 @@ const chk = document.getElementById('chk')
 
 chk.addEventListener('change', ()=>{
     document.body.classList.toggle('dark'),
-    document.querySelectorAll('.titulo, .nunito400, .subTitulo, .tituloIntroducao').forEach(element => {
+    document.querySelectorAll('.titulo, .nunito400, .subTitulo,.subTituloProjetos, .tituloIntroducao, .nomeTopo').forEach(element => {
         console.log(element)
         element.classList.toggle('darkTitulo')
+    })
+    document.querySelectorAll('.start, .end, .card').forEach(element => {
+        console.log(element)
+        element.classList.toggle('darkBg')
+    })
+    document.querySelectorAll('.btnTransparente, .btnLinkedin').forEach(element => {
+        console.log(element)
+        element.classList.toggle('darkBtn')
     })
    
 }) 
