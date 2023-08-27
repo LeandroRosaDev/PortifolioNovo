32e7d30ff893ed7e9d993be4b64972a608f3a87b
LeandroRosaDev <thivithale@hotmail.com>
2023-08-27 18:22:25 -0300

    Adicionando menu responsivo

--- script.js
+++ script.js
@@ -4,17 +4,40 @@ const chk = document.getElementById('chk')
 chk.addEventListener('change', ()=>{
     document.body.classList.toggle('dark'),
     document.querySelectorAll('.titulo, .nunito400, .subTitulo,.subTituloProjetos, .tituloIntroducao, .nomeTopo').forEach(element => {
-        console.log(element)
         element.classList.toggle('darkTitulo')
     })
     document.querySelectorAll('.start, .end, .card').forEach(element => {
-        console.log(element)
         element.classList.toggle('darkBg')
     })
     document.querySelectorAll('.btnTransparente, .btnLinkedin').forEach(element => {
-        console.log(element)
         element.classList.toggle('darkBtn')
     })
-   
 }) 
+
+// Script para ativar o menu hamburguer
+
+var abrirMenu = document.querySelector('.menuSeta')
+var fecharMenu = document.querySelector('.menuFechar')
+var navToSection = document.querySelectorAll('.menuLinks')
+
+abrirMenu.addEventListener('click', ()=>{
+    abrirMenu.style.display = 'none'
+    fecharMenu.style.display = 'block'
+    document.querySelector('.menuResponsivo').style.width = '100%',
+    document.querySelector('.menuSidebar').style.display = 'flex'
+})
+fecharMenu.addEventListener('click', ()=>{
+    fecharMenu.style.display = 'none'
+    abrirMenu.style.display = 'block'
+    document.querySelector('.menuResponsivo').style.width = '0%',
+    document.querySelector('.menuSidebar').style.display = 'none'
+})
+navToSection.forEach( function(link){
+    link.addEventListener('click', ()=>{
+        fecharMenu.style.display = 'none'
+        abrirMenu.style.display = 'block'
+        document.querySelector('.menuResponsivo').style.width = '0%',
+        document.querySelector('.menuSidebar').style.display = 'none'
+    })
+})
 
